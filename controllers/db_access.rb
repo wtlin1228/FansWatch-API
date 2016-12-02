95c82936e9057212f0fc17d9575f52e122bb8c06
wtlin1228 <wtlin0711@gmail.com>
2016-12-02 12:56:59 -0800

    fuck the codeship config....

--- controllers/db_access.rb
+++ controllers/db_access.rb
@@ -1,82 +1,82 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# # FansWatchAPI web service
-# class FansWatchAPI < Sinatra::Base
+# FansWatchAPI web service
+class FansWatchAPI < Sinatra::Base
 
-#   get "/#{API_VER}/db_page/?" do
-#     begin
-#       page = Page.all.first
+  get "/#{API_VER}/db_page/?" do
+    begin
+      page = Page.all.first
 
-#       content_type 'application/json'
-#       { id: page.id, fb_id: page.fb_id, name: page.name }.to_json
-#     rescue
-#       content_type 'text/plain'
-#       halt 404, "FB Page not found"
-#     end
-#   end
+      content_type 'application/json'
+      { id: page.id, fb_id: page.fb_id, name: page.name }.to_json
+    rescue
+      content_type 'text/plain'
+      halt 404, "FB Page not found"
+    end
+  end
 
-#   post "/#{API_VER}/forTest/?" do
-#     begin
-#       Group.create(fb_id: "1234", name: "Leo")
-#       Posting.create(
-#         fb_id: "test_fb_id_1" ,
-#         created_time: "test_created_time_1" ,
-#         message: "test_message_1" ,
-#         attachment_title: "test_attachment_title_1" ,
-#         attachment_description: "test_attachment_description_1" ,
-#         attachment_imgurl: "test_attachment_imgurl_1" ,
-#         attachment_url: "test_attachment_url_1"
-#       )      
-#       content_type 'text/plain'
-#       "generate test pattern"
-#     rescue
-#       content_type 'text/plain'
-#       halt 404, "Cannot generate test pattern"
-#     end
-#   end
+  post "/#{API_VER}/forTest/?" do
+    begin
+      Group.create(fb_id: "1234", name: "Leo")
+      Posting.create(
+        fb_id: "test_fb_id_1" ,
+        created_time: "test_created_time_1" ,
+        message: "test_message_1" ,
+        attachment_title: "test_attachment_title_1" ,
+        attachment_description: "test_attachment_description_1" ,
+        attachment_imgurl: "test_attachment_imgurl_1" ,
+        attachment_url: "test_attachment_url_1"
+      )      
+      content_type 'text/plain'
+      "generate test pattern"
+    rescue
+      content_type 'text/plain'
+      halt 404, "Cannot generate test pattern"
+    end
+  end
 
-#   # Body args (JSON) e.g.: {"url": "http://facebook.com/groups/group_name"}
-#   post "/#{API_VER}/db_page/?" do
-#     begin
-#       body_params = request.params
-#       fb_page_url = body_params['url'].to_s
-#       fb_page_id  = FansWatch::FbApi.page_id(fb_page_url)
+  # Body args (JSON) e.g.: {"url": "http://facebook.com/groups/group_name"}
+  post "/#{API_VER}/db_page/?" do
+    begin
+      body_params = request.params
+      fb_page_url = body_params['url'].to_s
+      fb_page_id  = FansWatch::FbApi.page_id(fb_page_url)
       
-#       fb_page = FansWatch::Page.find(id: fb_page_id)
+      fb_page = FansWatch::Page.find(id: fb_page_id)
       
-#       page_id = fb_page.id
-#       page_name = fb_page.name
-#       if Page.find(fb_id: page_id)
-#         halt 422, "Page (id: #{page_id}) already exists"
-#       end
+      page_id = fb_page.id
+      page_name = fb_page.name
+      if Page.find(fb_id: page_id)
+        halt 422, "Page (id: #{page_id}) already exists"
+      end
       
-#     rescue
-#       content_type 'text/plain'
-#       halt 404, "Cannot find page by url (url: #{fb_page_url} )"
-#     end
+    rescue
+      content_type 'text/plain'
+      halt 404, "Cannot find page by url (url: #{fb_page_url} )"
+    end
 
-#     begin
-#       db_page = Page.create(fb_id: page_id, name: page_name)
+    begin
+      db_page = Page.create(fb_id: page_id, name: page_name)
 
-#       fb_page.feed.postings.each do |fb_posting|
-#         Posting.create(
-#           page_id:       db_page.id,
-#           fb_id:         fb_posting.id,
-#           created_time:  fb_posting.created_time,
-#           message:       fb_posting.message,
-#           attachment_title:        fb_posting.attachment&.title,
-#           attachment_description:  fb_posting.attachment&.description,
-#           attachment_url:          fb_posting.attachment&.url,
-#           attachment_imgurl:       fb_posting.attachment&.image_url
-#         )
-#       end
+      fb_page.feed.postings.each do |fb_posting|
+        Posting.create(
+          page_id:       db_page.id,
+          fb_id:         fb_posting.id,
+          created_time:  fb_posting.created_time,
+          message:       fb_posting.message,
+          attachment_title:        fb_posting.attachment&.title,
+          attachment_description:  fb_posting.attachment&.description,
+          attachment_url:          fb_posting.attachment&.url,
+          attachment_imgurl:       fb_posting.attachment&.image_url
+        )
+      end
 
-#       content_type 'application/json'
-#       { page_id: db_page.id, name: db_page.name }.to_json
+      content_type 'application/json'
+      { page_id: db_page.id, name: db_page.name }.to_json
 
-#     rescue
-#       content_type 'text/plain'
-#       halt 500, "Cannot create page (id: #{page_id})"
-#     end
-#   end
-# end
+    rescue
+      content_type 'text/plain'
+      halt 500, "Cannot create page (id: #{page_id})"
+    end
+  end
+end
--- spec/db_access_spec.rb
+++ spec/db_access_spec.rb
@@ -1,90 +1,90 @@
-# # frozen_string_literal: true
-# require_relative 'spec_helper'
+# frozen_string_literal: true
+require_relative 'spec_helper'
 
-# describe 'DB ACCESS' do
+describe 'DB ACCESS' do
 
-#   before do
-#     VCR.insert_cassette DB_PAGES_CASSETTE, record: :new_episodes
-#   end
+  before do
+    VCR.insert_cassette DB_PAGES_CASSETTE, record: :new_episodes
+  end
 
-#   after do
-#     VCR.eject_cassette
-#   end
+  after do
+    VCR.eject_cassette
+  end
 
-#   describe '[DB]Find Page by its ID' do
-#     before do
-#       # Page.create(fb_id: "1234", name: "Leo")
-#       # Posting.create(
-#       #   fb_id: "test_fb_id_1" ,
-#       #   created_time: "test_created_time_1" ,
-#       #   message: "test_message_1" ,
-#       #   attachment_title: "test_attachment_title_1" ,
-#       #   attachment_description: "test_attachment_description_1" ,
-#       #   attachment_imgurl: "test_attachment_imgurl_1" ,
-#       #   attachment_url: "test_attachment_url_1"
-#       # )      
-#       # DB[:pages].delete
-#       # Page.create(fb_id: "1234", name: "Leo")
-#     end
-#     it 'HAPPY: should find a page given a correct id' do
-#       get "api/v0.1/db_page/?"
+  describe '[DB]Find Page by its ID' do
+    before do
+      # Page.create(fb_id: "1234", name: "Leo")
+      # Posting.create(
+      #   fb_id: "test_fb_id_1" ,
+      #   created_time: "test_created_time_1" ,
+      #   message: "test_message_1" ,
+      #   attachment_title: "test_attachment_title_1" ,
+      #   attachment_description: "test_attachment_description_1" ,
+      #   attachment_imgurl: "test_attachment_imgurl_1" ,
+      #   attachment_url: "test_attachment_url_1"
+      # )      
+      # DB[:pages].delete
+      # Page.create(fb_id: "1234", name: "Leo")
+    end
+    it 'HAPPY: should find a page given a correct id' do
+      get "api/v0.1/db_page/?"
 
-#       last_response.status.must_equal 200
-#       last_response.content_type.must_equal 'application/json'
-#       page_data = JSON.parse(last_response.body)
-#       # page_data['fb_id'].length.must_be :>, 0
-#       # page_data['name'].length.must_be :>, 0
-#     end
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+      page_data = JSON.parse(last_response.body)
+      # page_data['fb_id'].length.must_be :>, 0
+      # page_data['name'].length.must_be :>, 0
+    end
 
-#     it 'SAD: should report if a page is not found' do
-#       get "api/v0.1/page/0"
+    it 'SAD: should report if a page is not found' do
+      get "api/v0.1/page/0"
 
-#       last_response.status.must_equal 404
-#       last_response.body.must_include 'not found'
-#     end
-#   end
+      last_response.status.must_equal 404
+      last_response.body.must_include 'not found'
+    end
+  end
 
-#   describe '[DB]Post a page\'s url and it should get the page\'s name and id' do
-#     before do
-#       # Page.create(fb_id: "1234", name: "Leo")
-#       # Posting.create(
-#       #   fb_id: "test_fb_id_1" ,
-#       #   created_time: "test_created_time_1" ,
-#       #   message: "test_message_1" ,
-#       #   attachment_title: "test_attachment_title_1" ,
-#       #   attachment_description: "test_attachment_description_1" ,
-#       #   attachment_imgurl: "test_attachment_imgurl_1" ,
-#       #   attachment_url: "test_attachment_url_1"
-#       # )      
-#       # DB[:pages].delete
-#       # DB[:postings].delete
-#     end
-#     it 'HAPPY: should find the page' do
-#       post "api/v0.1/db_page/?url=#{HAPPY_PAGE_URL}"
+  describe '[DB]Post a page\'s url and it should get the page\'s name and id' do
+    before do
+      # Page.create(fb_id: "1234", name: "Leo")
+      # Posting.create(
+      #   fb_id: "test_fb_id_1" ,
+      #   created_time: "test_created_time_1" ,
+      #   message: "test_message_1" ,
+      #   attachment_title: "test_attachment_title_1" ,
+      #   attachment_description: "test_attachment_description_1" ,
+      #   attachment_imgurl: "test_attachment_imgurl_1" ,
+      #   attachment_url: "test_attachment_url_1"
+      # )      
+      # DB[:pages].delete
+      # DB[:postings].delete
+    end
+    it 'HAPPY: should find the page' do
+      post "api/v0.1/db_page/?url=#{HAPPY_PAGE_URL}"
 
-#       last_response.status.must_equal 200
-#       last_response.content_type.must_equal 'application/json'
-#       body = JSON.parse(last_response.body)
-#       body.must_include 'name'
-#       body.must_include 'page_id'
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+      body = JSON.parse(last_response.body)
+      body.must_include 'name'
+      body.must_include 'page_id'
 
-#       Page.count.must_equal 1
-#       Posting.count.must_be :>=, 10
-#     end
+      Page.count.must_equal 1
+      Posting.count.must_be :>=, 10
+    end
 
-#     it 'SAD: should report the page cannot be found' do
-#       post "api/v0.1/db_page/?url=#{SAD_PAGE_URL}"
+    it 'SAD: should report the page cannot be found' do
+      post "api/v0.1/db_page/?url=#{SAD_PAGE_URL}"
 
-#       last_response.status.must_equal 404
-#       last_response.body.must_include SAD_PAGE_URL
-#     end
+      last_response.status.must_equal 404
+      last_response.body.must_include SAD_PAGE_URL
+    end
 
-#     it 'should report error if group already exists' do
-#       2.times do
-#         post "api/v0.1/db_page/?url=#{HAPPY_PAGE_URL}"
-#       end
+    it 'should report error if group already exists' do
+      2.times do
+        post "api/v0.1/db_page/?url=#{HAPPY_PAGE_URL}"
+      end
 
-#       last_response.status.must_equal 422
-#     end
-#   end
-# end
+      last_response.status.must_equal 422
+    end
+  end
+end
